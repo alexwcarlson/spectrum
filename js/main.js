ac8b077b6d847b69b6cc26b3ad303b1ec0647747
alexwcarlson <awcarlson@gmail.com>
2017-01-18 22:58:58 -0600

    added fontawesome to sass files, styled nav, added jquery for navigation interaction, some styling for a navigation option, not yet full responsive

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.nav-button').click(function(){
+        $('.nav-menu').css('transform','scale(1,1)');
+
+    });
+$('.nav-close').click(function(){
+    $('.nav-menu').css('transform', 'scale(0,0)');
+});
+
+
+
+});
